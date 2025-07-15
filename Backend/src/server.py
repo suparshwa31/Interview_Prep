7a1b62a751718547c33b25cc7c2a073a36f34c35
suparshwa31 <patil311299@gmail.com>
2025-07-15 12:39:54 -0700

    changed server.py location

--- Backend/src/server.py
+++ Backend/src/server.py
@@ -1,9 +1,8 @@
-import os
-from src.app import app
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+from app import app
+    import uvicorn
+    import os
 
 if __name__ == "__main__":
-    import uvicorn
+
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
