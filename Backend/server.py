5f9acc72a86d9749c757c56ac7c87e319d18db24
suparshwa31 <patil311299@gmail.com>
2025-07-15 12:33:51 -0700

    resolved deployment issue

--- Backend/server.py
+++ Backend/server.py
@@ -1,6 +1,8 @@
 import os
 from src.app import app
 
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
